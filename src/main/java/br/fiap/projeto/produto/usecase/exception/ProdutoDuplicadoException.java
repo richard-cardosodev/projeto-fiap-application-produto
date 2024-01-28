64b7c5c7f00fe6bb1d1b7a2651e9746a019ec83b
Mario Sclavo <mmsclavo@gmail.com>
2024-01-28 02:34:30 -0300

    feat: inclusao de caso de teste de duplicidade

--- src/main/java/br/fiap/projeto/produto/usecase/exception/ProdutoDuplicadoException.java
+++ src/main/java/br/fiap/projeto/produto/usecase/exception/ProdutoDuplicadoException.java
@@ -3,9 +3,6 @@ package br.fiap.projeto.produto.usecase.exception;
 public class ProdutoDuplicadoException extends BaseException {
     private static final String DEFAULT_MESSAGE = "Produto já existe!";
     private static final int CODE = 3002;
-    public ProdutoDuplicadoException() {
-        super(CODE, DEFAULT_MESSAGE);
-    }
     public ProdutoDuplicadoException(String message) {
         super(CODE, message);
     }
